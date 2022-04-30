c047208359643dab8793a97ab15c9940682df5d1
prajwalmahajan101 <82263793+prajwalmahajan101@users.noreply.github.com>
2022-04-30 12:24:09 +0530

    Finalizing Project

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const display = document.getElementById("clock");
+const button = document.getElementById("alarmButton");
+
 const list = document.getElementById("alarmList");
 const timeControl = document.querySelector('input[type="time"]');
 
@@ -18,11 +20,15 @@ const AlarmList = [
 const updateAlarmList = () => {
   let data = "";
   let k = 0;
+  if (AlarmList.length == 0) {
+    list.innerHTML = "<h3>NO Alarm Set Till Now</h3>";
+    return;
+  }
   for (i of AlarmList) {
     data +=
       "<li id='" +
       k +
-      "' onclick='alramDelete(this.id)'>" +
+      "' onclick='alarmDelete(this.id)'>" +
       i[0] +
       " hr " +
       i[1] +
@@ -40,8 +46,13 @@ const updateTime = () => {
   const minutes = formatTime(date.getMinutes());
   const seconds = formatTime(date.getSeconds());
 
-  if (checkList([hour, minutes])) audio.play();
-  else audio.pause();
+  if (checkList([hour, minutes])) {
+    audio.play();
+    setButton();
+  } else {
+    audio.pause();
+    removeButton();
+  }
 
   display.innerText = `${hour} : ${minutes} : ${seconds}`;
 };
@@ -77,7 +88,7 @@ const checkList = (data) => {
   return false;
 };
 
-const alramDelete = (index) => {
+const alarmDelete = (index) => {
   //   console.log(index);
 
   if (confirm("Do You want to delete this alarm :")) {
@@ -85,4 +96,47 @@ const alramDelete = (index) => {
     //   console.log(AlarmList);
     updateAlarmList();
   } else return;
+};
+
+const clearHandler = () => {
+  audio.pause();
+  const date = new Date();
+  const hour = formatTime(date.getHours());
+  const minutes = formatTime(date.getMinutes());
+
+  for (let i = 0; i < AlarmList.length; i++) {
+    if (AlarmList[i][0] == hour && AlarmList[i][1] == minutes) {
+      AlarmList.splice(i, 1);
+    }
+  }
+  updateAlarmList();
+  removeButton();
+};
+
+const snoozeHandler = () => {
+  audio.pause();
+  const date = new Date();
+  const hour = formatTime(date.getHours());
+  const minutes = formatTime(date.getMinutes());
+  for (let i = 0; i < AlarmList.length; i++) {
+    if (AlarmList[i][0] == hour && AlarmList[i][1] == minutes) {
+      console.log(typeof AlarmList[i][1]);
+      let temp = parseInt(AlarmList[i][1]);
+      console.log(typeof temp + " " + temp);
+      temp += 5;
+      temp += "";
+      AlarmList[i][1] = temp;
+    }
+  }
+  updateAlarmList();
+
+  removeButton();
+};
+
+const setButton = () => {
+  button.innerHTML =
+    '<button class="controls" onclick="clearHandler()">Stop alarm</button><button class="controls" onclick="snoozeHandler()">Snooze alarm</button>';
+};
+const removeButton = () => {
+  button.innerHTML = "";
 };
